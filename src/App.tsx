171b4367df04f36b58a8c94e870f893ebdd89e11
Juan Salazar <juanedsa@gmail.com>
2024-03-06 19:15:52 -0500

    🎨 Configuracion inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import "./App.css";
+import './App.css'
 
 function App() {
   return (
     <>
-      <h1>Juan y Andre</h1>
+      <h1>Nos Casamos!</h1>
+      <h2>Juan y Andre</h2>
+      <h3>05 de Octubre de 2024</h3>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
