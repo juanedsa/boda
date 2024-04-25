656c1a08a9dadc87bcd46c662dcfcd9c46d3eaff
Juan Salazar <juanedsa@gmail.com>
2024-04-25 09:40:29 -0500

    Add Images

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,8 +5,17 @@ function Header() {
     <section className="header">
       {/* <h1 className="header__title">Juan y Andrea</h1>
       <p className="header__date">05 - Octubre - 2024</p> */}
-      <img src="portada.png" />
-      <img src="logo.png" />
+      <img src="1.png" />
+      <img src="2.png" />
+      <img src="3.png" />
+      <img src="4.png" />
+      <img src="5.png" />
+      <img src="6.png" />
+      <img src="7.png" />
+      <img src="8.png" />
+      <img src="9.png" />
+      <img src="10.png" />
+      <img src="11.png" />
     </section>
   )
 }
