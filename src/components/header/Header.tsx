48c540c7d1c2443f855c8778bea70667a8f5edf6
Juan Salazar <juanedsa@gmail.com>
2024-04-27 18:51:13 -0500

    :bento: Update Assetss

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,6 +16,11 @@ function Header() {
       <img src="9.png" />
       <img src="10.png" />
       <img src="11.png" />
+      <img src="12.png" />
+      <img src="13.png" />
+      <img src="14.png" />
+      <img src="15.png" />
+      <img src="16.png" />
     </section>
   )
 }
