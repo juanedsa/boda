33930eb669cf2223ee2ad4cca1667139f2687b71
Juan Salazar <juanedsa@gmail.com>
2024-05-01 09:43:10 -0500

    fix background and add footer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <>
       <Header />
+      <div className='footer'>
+        <p>Derechos de autor © 2024. Todos los derechos reservados. Creado con amor y pasión por Andre y Juan. ¡Gracias por visitarnos!</p>
+      </div>
     </>
   )
 }
--- src/components/countdown/Countdown.tsx
+++ src/components/countdown/Countdown.tsx
@@ -6,7 +6,7 @@ function Countdown() {
   const [timeLeft, setTimeLeft] = useState(getTimeLeft())
 
   function getTimeLeft() {
-    const targetDate: number = new Date('2024-10-05T00:00:00').getTime()
+    const targetDate: number = new Date('2024-10-05T16:00:00').getTime()
     const now = new Date().getTime()
     const diff = targetDate - now
 
